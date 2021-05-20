56eb5709b9e75fd68c7b79f76ac43c8d4cbef854
James <j.e.raubenheimer@gmail.com>
2021-05-20 20:10:15 +0200

    Edited Allowed Hosts

--- django_afrisales_project/django_afrisales_project/settings.py
+++ django_afrisales_project/django_afrisales_project/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = 'django-insecure-*jg(88r-i_!t$-5$y4&c80++%daq&tn+!!!0fx8tj9-!z+hv*e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'afri-sales.herokuapp.com'
+    ]   
 
 
 # Application definition

a0ff56a65e739feec6c5542a01068558e109ee40
James <j.e.raubenheimer@gmail.com>
2021-05-27 20:34:36 +0200

    Production and development settings split up

--- django_afrisales_project/django_afrisales_project/settings/production.py
+++ django_afrisales_project/django_afrisales_project/settings/production.py
@@ -1,12 +1,16 @@
 from django_afrisales_project.settings.common import *
+import django_heroku
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*jg(88r-i_!t$-5$y4&c80++%daq&tn+!!!0fx8tj9-!z+hv*e'
-os.environ['SECRET_KEY']
+SECRET_KEY = os.environ['SECRET_KEY']
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = [
     'afri-sales.herokuapp.com'
-    ] 
+    ]
+
+django_heroku.settings(locals())
